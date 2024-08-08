6f25facaed4aa14682b1b340ce08ff6239440706
Kevin Lau <kevin@1stcollab.com>
2024-08-08 11:39:59 -0400

    Add cmake

---
+++ build/CMakeFiles/cpplox.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpplox.dir/src/interpreter.cpp.o"
+  "CMakeFiles/cpplox.dir/src/interpreter.cpp.o.d"
+  "CMakeFiles/cpplox.dir/src/lox.cpp.o"
+  "CMakeFiles/cpplox.dir/src/lox.cpp.o.d"
+  "CMakeFiles/cpplox.dir/src/loxclass.cpp.o"
+  "CMakeFiles/cpplox.dir/src/loxclass.cpp.o.d"
+  "CMakeFiles/cpplox.dir/src/loxinstance.cpp.o"
+  "CMakeFiles/cpplox.dir/src/loxinstance.cpp.o.d"
+  "CMakeFiles/cpplox.dir/src/parser.cpp.o"
+  "CMakeFiles/cpplox.dir/src/parser.cpp.o.d"
+  "CMakeFiles/cpplox.dir/src/resolver.cpp.o"
+  "CMakeFiles/cpplox.dir/src/resolver.cpp.o.d"
+  "CMakeFiles/cpplox.dir/src/scanner.cpp.o"
+  "CMakeFiles/cpplox.dir/src/scanner.cpp.o.d"
+  "CMakeFiles/cpplox.dir/src/token.cpp.o"
+  "CMakeFiles/cpplox.dir/src/token.cpp.o.d"
+  "cpplox"
+  "cpplox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpplox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
